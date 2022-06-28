ebfdcbbaec1d4f32802c73ed1008049d9d29983f
Anton_arl <CelestialArlate@gmail.com>
2022-06-28 15:46:02 +0700

    Fix case 22 _1

--- Program.cs
+++ Program.cs
@@ -73,7 +73,8 @@ while (begin)
         case 22:
             // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
             Console.Write("Введите любое число: ");
-            string numStr = Console.ReadLine();
+            num = Convert.ToInt32(Console.ReadLine());
+            string numStr = Convert.ToString(num);
             numDigit = numStr.Length;
             if (numDigit > 2) Console.WriteLine($"Третья цифра введённого числа: {numStr[2]}");
             else Console.WriteLine("третьей цифры нет");
